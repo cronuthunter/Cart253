98d804b1f76dee91a4a686462fcd533c9d7d09be
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-09-28 14:52:53 -0400

    Ex: Started Dodge-em exercise

---
+++ dodge-em/js/script.js
@@ -0,0 +1,33 @@
+/**
+Dodge Em
+
+Claire Amirault
+Cart 253B
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
