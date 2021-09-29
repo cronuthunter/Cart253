992f057bfd62d23f3fe6f1e72a6e16865b4e24a6
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-09-28 22:18:23 -0400

    live: activity done

--- dodge-em/js/script.js
+++ dodge-em/js/script.js
@@ -5,29 +5,76 @@ Claire Amirault
 Cart 253B
 
 */
+let bg ={
+   r:0,
+   g:0,
+   b:0,
+   w:1000,
+   h:720,
+   };
 
-"use strict";
+ let covid19 ={
+   x:0,
+   y:250,
+   size:250,
+   speed:10,
+   r:250,
+   };
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
+ let user ={
+   size: 100,
+   rgb:250,
+   };
+///////////////////
 function setup() {
+   createCanvas(bg.w,bg.h);
+   noCursor();
 
-}
+   //set the value ONCE at the start
+   covid19.y=random(720);
+   };
 
 
-/**
-Description of draw()
-*/
 function draw() {
 
-}
+   background(bg.r,bg.g,bg.b);
+
+   //static
+   for( let pts =0; pts<=1000; pts++){
+
+       stroke('white');
+       point(random(bg.w),random(bg.h));
+       };
+
+   //move and draw covid
+   covid19.x=covid19.x+covid19.speed;
+     if(covid19.x>=bg.w){
+       covid19.x=0;
+       covid19.y=random(720);
+       covid19.speed+=2;
+       if(covid19.speed>=30){covid19.speed=30};
+     };
+
+   noStroke();
+   fill(covid19.r,0,0);
+   ellipse(covid19.x,covid19.y,covid19.size,covid19.size);
+
+   //draw user mouse
+   noStroke();
+   fill(user.rgb,user.rgb,user.rgb);
+   ellipse(mouseX,mouseY,user.size,user.size);
+
+   //dot intersection
+
+   let d = int(dist(covid19.x,covid19.y,mouseX,mouseY));
+   if(d <= covid19.size/2 + user.size/2){
+     noLoop();
+
+     fill(250);
+     textAlign(CENTER);
+     textSize(50);
+     text('Covid Touch! EW! Your Score:'+ covid19.speed,bg.w/2,bg.h/2)
+   };
+
+   console.log(d);
+   }
