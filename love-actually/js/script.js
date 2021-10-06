ab9299c99404734333f4fc7afb7fb709a96bec04
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-10-06 16:11:14 -0400

    E2: not very cat themed
    tried to turn one of the circles into a cat but it was a little too advanced for me.

--- love-actually/js/script.js
+++ love-actually/js/script.js
@@ -1,5 +1,7 @@
 /**
+CLaire Amirault - Cart 253B
 
+A simple program about finding a cat that uses key inputs.
 */
 //Objects
 let circle1={
@@ -24,13 +26,13 @@ let circle2={
 
 let state = 'title'; // start with title, mouse click will change it to simulation then output love or sadness
 ////////////////main//////////////////////////////
+function preload(){
+  //img=loadImage('images/catHead.png')
+}
 
 function setup() {
 createCanvas(500,500);
 setupCircles();
-
-
-
 
 function setupCircles(){
   //position circles set seprate from each other
@@ -71,10 +73,10 @@ function draw() {
 
 function title() {
   push();
-  textSize(64);
+  textSize(50);
   fill(200,100,100);
   textAlign(CENTER,CENTER);
-  text('LOVE?', width/2,height/2);
+  text('Look For A Cat?', width/2,height/2);
   textSize(30);
   text('Press any key to start', width/2 , height/1.5);
   pop();
@@ -89,19 +91,22 @@ function simulation(){
 
 function love(){
   push();
-  textSize(64);
+  textSize(50);
   fill(255,0,0);
   textAlign(CENTER,CENTER);
-  text('LOVE!<3', width/2,height/2);
+  text('You Found a Kitty!!', width/2,height/2);
+  text('<3', width/2, height/1.5);
   pop();
 }
 
 function sadness(){
   push();
-  textSize(64);
+  textSize(30);
   fill(150,150,255);
   textAlign(CENTER,CENTER);
-  text('not a match:(', width/2,height/2);
+  text('That is not the cat for you.', width/2,height/2);
+  textSize(40)
+  text('Try Again!', width/2, height/1.5);
   pop();
 }
 
@@ -131,9 +136,18 @@ function checkOverlap(){
 
 
 function display(){
+  //add photo to ellipse, could not figure this part out, left it in incase you wanted to take a look at my thinking.
+  //let photo,maskImage
+  //preload();
+  //image(photo,0,0);
+ //maskImage = createGraphics(300, 200)
+  //maskImage.ellipse(circle2.x,circle2.y, circle2.size);
+  //photo.mask(maskImage);
+  //image(photo, 300,0)
   //display circles
-  ellipse(circle1.x,circle1.y, circle1.size);
   ellipse(circle2.x,circle2.y, circle2.size);
+  ellipse(circle1.x,circle1.y, circle1.size);
+
 }
 
 function keyPressed(){
