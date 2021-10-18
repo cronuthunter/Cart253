efc490de7f999a4991b48e94cfe676fb501e7a8d
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-10-18 03:13:09 -0400

    P1: generating + moving zombies down

--- project1/js/script.js
+++ project1/js/script.js
@@ -13,6 +13,8 @@ author, and this description to match your project!
 var player = document.getElementById("player");
 var screen = document.getElementById("screen");
 //////////
+
+//key move
 window.addEventListener("keydown",(e)=>{
 var left= parseInt(window.getComputedStyle(player).getPropertyValue("left"));
   if (e.key=="ArrowLeft" && left > 0){
@@ -21,17 +23,36 @@ var left= parseInt(window.getComputedStyle(player).getPropertyValue("left"));
   else if (e.key=="ArrowRight" && left >= 460) {
     player.style.left=left+10+"px";
   }
-}
+});
 
+function simulation() {
 
-var generateZombie = setInterval())=>{
-  var zombie = document.createElement("div");
-  zombie.classList.add("zombie");
-  var zombieLeft =parseInt(window.getComputedStyle(player).getPropertyValue("left"));
-  zombie.style.left =Math.
-}
+  var generateZombie = setInterval())=>{
+    var zombie = document.createElement("div");
+    zombie.classList.add("zombie");
+    //zombie appears ramdomly between 0 + 460
+    var zombieLeft =parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+    zombie.style.left =Math.floor(Math.random()*460) + "px";
 
+    screen.appendChild(rock);
+  }, 1500);
 
+  var moveZom = setInterval(() =>{
+    var zombie = document.getElementById("zombie");
+
+    if (zombie!=undefined){
+      for (var i=0;i<rocks.length;i++){
+        //moving zombies down
+        var zombie=zombie[i]
+        var zomTop = parseInt(
+        window.getComputedStyle(zombie).getPropertyValue("top"));
+        zombie.style.top=zomTop + 20 + "px";
+
+      }
+    }
+  }, 450);
+
+}//sim end
 
 
 
