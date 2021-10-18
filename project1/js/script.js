9b6fee1670090362760f02205ce8079e0a5a4206
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-10-18 16:03:38 -0400

    P1: bullets remove zombies after hit

--- project1/js/script.js
+++ project1/js/script.js
@@ -6,6 +6,8 @@ This is a zombie apocalypse simulation game. Player moves with the arrow keys an
 shoots the zombies with the space bar. The simulation is set in an abandon town
 overun by zombies and you must save your group from the heard.
 
+The program is a bit more complex then what we have done in class so I used HTML
+and CSS to do this project becasue I am more fimilar with those.
 *all graphics done by me
 
 */
@@ -27,13 +29,34 @@ var left= parseInt(window.getComputedStyle(player).getPropertyValue("left"));
   else if (e.key=="ArrowRight" && left >= 460) {
     player.style.left=left+10+"px";
   }
-  if ((e.key= "ArrowUp" || e.keyCode == 323)){
+  if ((e.key= "ArrowUp" || e.keyCode == 32) {
     var bullet = document.createElement("div");
     screen.appendChild(bullet);
-
+    //bullets
     var moveBullet = setInterval(()=>
-    var bulletBottom = parseInt(window.getComputedStyle(player)).getPropertyValue("left"));
-  },50);
+      //destroying zombies
+      var zombie = document.getElementsByClassName('zombie');
+      for (var i=0;i<zombie.length;i++){
+        var zombie = zombie[i];
+
+        var zombieBound = zombie.getBoundingClientRect();
+        var bulletBound = bullet.getBoundingClientRect();
+
+        if (bulletBound.left >= zombieBound.left && bulletBound.right >= zombieBound.right
+         bulletBound.top <= zombieBound.top && bulletBound.bottom <= zombieBound.bottom){
+           zombie.parentElement.removeChild(zombie); //removes hit zombie
+
+         }
+
+
+      }
+
+      var bulletBottom = parseInt(window.getComputedStyle(bullet).getPropertyValue("bottom"));
+    );
+    bullet.style.left=left + "px";
+    bullet.style.bottom = bulletBottom + 3 + "px";
+    },50);
+  }
 });
 
 function simulation() {
@@ -45,14 +68,14 @@ function simulation() {
     var zombieLeft =parseInt(window.getComputedStyle(player).getPropertyValue("left"));
     zombie.style.left =Math.floor(Math.random()*460) + "px";
 
-    screen.appendChild(rock);
+    screen.appendChild(zombie);
   }, 1500);
 
   var moveZom = setInterval(() =>{
     var zombie = document.getElementById("zombie");
 
     if (zombie!=undefined){
-      for (var i=0;i<rocks.length;i++){
+      for (var i=0;i<zombie.length;i++){
         //moving zombies down
         var zombie=zombie[i]
         var zomTop = parseInt(
