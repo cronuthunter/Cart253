4fc9bf104ed711401556fe32da6e22fe58983042
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-10-20 02:27:05 -0400

    P1: art studio simulation

--- Projects/project1/js/script.js
+++ Projects/project1/js/script.js
@@ -1,17 +1,22 @@
 /**
-Title of Project
-Author Name
+Gather Simulation
+Claire Amirault
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Art Studio Simulation!
+-----------------------------------
+program complexity:
+- draw on canvas
+-save image of canvas
+-change canvas background
+-
 */
+//varibles
+
 
 "use strict";
+let state='simulation';
 
 
-/**
-Description of preload
-*/
 function preload() {
 
 }
@@ -21,13 +26,44 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+createCanvas(1024,768);
 }
 
 
 /**
 Description of draw()
 */
-function draw() {
+function draw(){
+
+if (state==='title'){
+  title();
+}
+
+else if (state==='simulation') {
+  simulation1();
+}
+
+}//end draw
+
+
+function title() {
+  push();
+  textSize(40);
+  fill(200,100,100);
+  textAlign(CENTER,CENTER);
+  text('Welcome to Art Studio Simulation!', width/2,height/2);
+  pop();
+}
+
+
+function simulation1(){
+if(mouseIsPressed==true){
+  stroke(mouseX,0,mouseY);
+  strokeWeight(20);
+  point(mouseX-5,mouseY-5);
+  point(mouseX*-1+600, mouseY-5);
+  point(mouseX*-1+600, mouseY*-1+600);
+  point(mouseX-5,mouseY*-1+400);
 
 }
+}//end simulation
