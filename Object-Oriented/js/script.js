e37763f8f478002af4bc1cfa657b98079f3ec163
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-10-26 00:40:28 -0400

    E5: paddle + ball class

--- Object-Oriented/js/script.js
+++ Object-Oriented/js/script.js
@@ -8,6 +8,14 @@ author, and this description to match your project!
 
 "use strict";
 
+//varibles
+let gravityForce = 0.0025;
+
+let paddle;
+
+let balls = [];
+let numBalls = 10;
+
 
 /**
 Description of preload
@@ -21,6 +29,16 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth,windowHeight);
+
+  paddle = new Paddle(300,20);
+
+  for (let i = 0; i < numBalls; i++) {
+    let x = random(0,width);
+    let y = random(-400,-100);
+    let ball = new Ball(x,y);
+    balls.push(ball);
+  }
 
 }
 
@@ -29,5 +47,8 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
 }
