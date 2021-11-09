f24757eef4e5a3f41ab8aca438b6ce8e9e13b9f7
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-11-08 19:58:44 -0500

    E10: done activity

--- Make-Some-Noise/js/Ball.js
+++ Make-Some-Noise/js/Ball.js
@@ -1,6 +1,6 @@
 class Ball{
 
-  constructor(x,y){
+  constructor(x,y,note){
     this.x=x;
     this.y=y;
     this.size=50;
@@ -12,27 +12,53 @@ class Ball{
     this.speed=3;
     this.vx= random(-this.speed,this.speed);
     this.vy=random(-this.speed,this.speed);
-    }
+
+    // Oscillator
+    this.oscillator = new p5.Oscillator();
+    this.nearFreq = 220;
+    this.farFreq = 440;
+    this.oscillator.amp(0.025);
+    this.oscillator.start();
+
+    // Synth
+    this.note = note;
+    this.synth = new p5.PolySynth();
+
+  }//end constructor
+
   move(){
     this.x += this.vx;
-    this.y += this.yy;
+    this.y += this.vy;
+
+    // Update frequency
+      let d = dist(this.x,this.y,width/2,height/2);
+      let maxDist = dist(0,0,width/2,height/2);
+      let newFreq = map(d,0,maxDist,this.nearFreq,this.farFreq);
+      this.oscillator.freq(newFreq);
   }
 
   bounce(){
     //physics of the bouce + check edge instead of center
     if (this.x - this.size/2 <0 || this.x + this.size/2 > width){
       this.vx = -this.vx;
+      this.playNote();
     }
     if (this.y - this.size/2 <0 || this.y + this.size/2 > width){
       this.vy = -this.vy;
+      this.playNote();
   }
+}
+  playNote() {
+   this.synth.play(this.note,0.4,0,0.1);
+ }
 
-  display(){
-    push();
-    noStroke();
-    fill(this.fill.r,this.fill.g,this.fill.b);
-    ellipse(this.x,this.y,this.size);
-    pop();
-  }
+ display() {
+     push();
+     noStroke();
+     fill(this.fill.r,this.fill.g,this.fill.b);
+     ellipse(this.x,this.y,this.size);
+     pop();
+   }
+
 
 }//end ball class
--- Make-Some-Noise/js/script.js
+++ Make-Some-Noise/js/script.js
@@ -7,19 +7,18 @@ This is a musical toy program, a program that plays sound on primitive physics!
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
+//vars
+let balls =[];
+// F-minor
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`,`C4`,`Db4`,`Eb4`,`F4`];
 
 /**
 Description of setup
 */
 function setup() {
+createCanvas(600,600);
+
+userStartAudio();
 
 }
 
@@ -28,5 +27,22 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  for (let i = 0; i < balls.length; i++){
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}// end of draw
+
+function mousePressed(){
+  createBall(mouseX,mouseY);
+}
+
+function createBall(x,y) {
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
