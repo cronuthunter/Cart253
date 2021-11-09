c6f64d817b1aea73c8a3e0d4710c2d2a49156d5c
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-11-08 19:05:08 -0500

    E10: Setting up w pippin
    following along with pippins video

---
+++ Make-Some-Noise/js/script.js
@@ -0,0 +1,32 @@
+/**
+Musical Toy
+
+Claire Amirault
+This is a musical toy program!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
