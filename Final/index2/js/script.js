134567a7990d3b302aaffa10f7ab308ac809f52a
cronuthunter <90426533+cronuthunter@users.noreply.github.com>
2021-12-09 21:11:42 -0500

    begin the drag and drop
    tested a few ways for drag and drop, I think this way will be the cleanest

--- Final/index2/js/script.js
+++ Final/index2/js/script.js
@@ -7,27 +7,43 @@ author, and this description to match your project!
 */
 
 "use strict";
+var dragValue;
+
+//
+
+function move(){
+  face1();
+  face2();
+
+  document.onmouseup=function(e){
+      dragValue=null;
+  }
+
+  document.onmousemove=function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    dragValue.style.left = x + "px";
+    dragValue.style.top = y + "px";
+
+  }
+
+}//end move
 
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
 
 
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+function face1(id){
+  var f1 = document.getElementById('f1');
+  f1.style.position= "absolute";
+  f1.onmousedown= function(){
+    dragValue = f1;
+  }
+}//f1
+function face2(id){
+  var f2 = document.getElementById('f2');
+  f2.style.position= "absolute";
+  f2.onmousedown= function(){
+    dragValue = f2;
+  }
+}//f2
